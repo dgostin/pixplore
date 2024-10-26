60e1b667f3e5a040132024bd2681d137bb617e3e
David Gostin <dgostin@gmail.com>
2024-10-26 09:10:41 -0400

    next

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Header = () => {
   return (
     <header className="flex justify-center">
-      <img src="/pixplore.jpg" className="h-16 w-16 mr-3" />
-      <h1 className="text-6xl mb-3 font-fi">PIXPLORE</h1>
+      <img src="/pixplore.jpg" className="h-8 w-8 sm:h-16 sm:w-16 mr-3" />
+      <h1 className="text-2xl sm:text-6xl mb-3 font-fi">PIXPLORE</h1>
     </header>
   );
 };
